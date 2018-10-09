ffe2a1553bbf07adb2425c79c4bef63da4eaed36
Tatiana Slonimskaia <slonimskaia@gmail.com>
2018-10-09 11:51:59 +0200

    Add a new lines

--- gittie.py
+++ gittie.py
@@ -1,6 +1,13 @@
-<<<<<<< HEAD
 print('Hello, Im Gittie')
 print('Hello!')
 print('This is so funny')
 print('there is so loud, dont shout')
+
+a = input("Enter a number:")
+
+if a == 1:
+    print("You are winner!")
+else:
+    print("You are not winner")
+
 
